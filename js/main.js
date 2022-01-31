d058e816e165c9955fd40a2f4831f9314940c6a7
DodgeShadow <bvoteam22@gmail.com>
2022-02-01 01:24:20 +0600

    add table sort

--- js/main.js
+++ js/main.js
@@ -4,11 +4,16 @@ $.ajax({
 }).done(function (response) {
     for (let i = 0; i < response.length; i++) {
         let country = response[i];
-        $('.table').append($('<div>').addClass('code').text(country.cca2))
-        $('.table').append($('<div>').addClass('flag').append($('<img>').addClass('flag__img').attr('src', country.flags.png)))
-        $('.table').append($('<div>').addClass('name').text(country.name.common))
-        $('.table').append($('<div>').addClass('capital').text(country.capital))
-        $('.table').append($('<div>').addClass('population').text(country.population))
+        $('tbody').append($('<tr>').addClass('row-' + i))
+        $('.row-' + i).append($('<td>').addClass('code').text(country.cca2))
+            .append($('<td>').addClass('flag').append($('<img>').addClass('flag__img').attr('src', country.flags.png)))
+            .append($('<td>').addClass('name').text(country.name.common))
+            .append($('<td>').addClass('capital').text(country.capital))
+            .append($('<td>').addClass('population').text(country.population))
         console.log(country);
     };
+    $(document).ready(function () {
+        $(".table").tablesorter();
+    });
 });
+
